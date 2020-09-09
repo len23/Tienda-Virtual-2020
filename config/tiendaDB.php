71cd335c2198e6f3c0a158aebbc0cd97ef759b0c
len23 <lenin.montalvo.23@hotmail.com>
2020-09-09 08:35:42 -0500

    Get products from database

--- config/tiendaDB.php
+++ config/tiendaDB.php
@@ -4,23 +4,27 @@
       include "coneccion.php";
 
       $query = "SELECT * FROM PRODUCTO";
-      $result = mysqli_query($enlace,$query);
+      
+      if(!$result = mysqli_query($enlace,$query)){
+        echo '<p>Hubo un error</p>';
+      }
+      /* var_dump(mysqli_fetch_array($result, MYSQLI_ASSOC)); */
 ?>
 
     <div class="row border-bottom py-5">
       <?php
         $i=1;
         /* while ($result_ar=mysqli_fetch_assoc($result)) { */
-        while ($i<=7) {
+        while ($result_ar = mysqli_fetch_assoc ($result)) {
       ?>    
-        <div class="col-md-4 text-center bg-white mt-3">
-          <img class="w-75" src="./images/compu-1.jpg" alt="">
+        <div class="col-md-4 text-center bg-white mt-3 border-right border-left pb-3">
+          <img class="w-75" src="<?php echo $result_ar['IMAGE']?>" alt="">
           <p class="description border-top border-bottom py-3">
-            COP. HP AIO ProOne 400 G5 i7-9700 8GB 1TB 23.8Inc HDMI 2USB3.1 FREE DOS Black
+            <?php echo $result_ar['DESCRIPCION']?>
           </p>
           <div class="info-shop d-flex justify-content-between align-items-center px-3 flex-wrap">
             <button class="btn btn-info">COMPRAR <i class="fas fa-shopping-cart"></i></button>
-            <span class="price">$887.78</span>
+            <span class="price">$<?php echo $result_ar['PRECIO']?></span>
           </div>
         </div>
       <?php
