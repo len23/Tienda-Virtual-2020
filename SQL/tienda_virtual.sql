9844eef95fa3634bc8174e6da8842f3c9a5b1f5e
len23 <lenin.montalvo.23@hotmail.com>
2020-09-15 21:55:41 -0500

    Add SQL

--- SQL/tienda_virtual.sql
+++ SQL/tienda_virtual.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 15, 2020 at 05:01 PM
+-- Generation Time: Sep 16, 2020 at 02:54 AM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -38,56 +38,6 @@ CREATE TABLE IF NOT EXISTS `carrito` (
   KEY `producto_id` (`producto_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `carrito`
---
-
-INSERT INTO `carrito` (`carrito_id`, `user_id`, `producto_id`, `cantidad`) VALUES
-(55, 11, 1, 2),
-(56, 11, 3, 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cliente`
---
-
-DROP TABLE IF EXISTS `cliente`;
-CREATE TABLE IF NOT EXISTS `cliente` (
-  `CLIENTE_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `NOMBRE_` varchar(30) NOT NULL,
-  `CLIENTE_CED` decimal(10,0) NOT NULL,
-  `CLIENTE_TLFCONVENCIONAL` decimal(15,0) NOT NULL,
-  `CLIENTE_TLFCELULAR` decimal(15,0) DEFAULT NULL,
-  `CLIENTE_DIRECCION` varchar(50) NOT NULL,
-  `CLIENTE_EMAIL` varchar(30) NOT NULL,
-  `CLIENTE_ESTADO` varchar(30) NOT NULL,
-  PRIMARY KEY (`CLIENTE_ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `compra`
---
-
-DROP TABLE IF EXISTS `compra`;
-CREATE TABLE IF NOT EXISTS `compra` (
-  `COMPRA_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `PROVEEDOR_ID` int(11) NOT NULL,
-  `PROVEEDOR_RUC` varchar(30) NOT NULL,
-  `PRODUCTO_ID` int(11) NOT NULL,
-  `INVENTARIO_ID` int(11) NOT NULL,
-  `COMPRA_NROFACTURA` decimal(10,0) NOT NULL,
-  `COMPRA_VALORBASE` float NOT NULL,
-  `COMPRA_IVA` float NOT NULL,
-  `COMPRA_VALORTOTAL` float NOT NULL,
-  PRIMARY KEY (`COMPRA_ID`),
-  KEY `FK_COMPRA_PRODUCTO` (`PRODUCTO_ID`),
-  KEY `FK_INVENTARIO_COMPRA` (`INVENTARIO_ID`),
-  KEY `FK_PROVEEDOR_COMPRA` (`PROVEEDOR_ID`,`PROVEEDOR_RUC`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 -- --------------------------------------------------------
 
 --
@@ -96,34 +46,16 @@ CREATE TABLE IF NOT EXISTS `compra` (
 
 DROP TABLE IF EXISTS `detalle_factura`;
 CREATE TABLE IF NOT EXISTS `detalle_factura` (
-  `DETALLEFAC_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `PRODUCTO_ID` int(11) NOT NULL,
-  `FACTURA_ID` int(11) NOT NULL,
-  `DETALLEFAC_NROUNIDADES` decimal(10,0) NOT NULL,
-  `DETALLEFAC_VALORUNITARIO` float NOT NULL,
-  `DETALLEFAC_IVA` float NOT NULL,
-  `DETALLEFAC_VALORTOTAL` float NOT NULL,
-  PRIMARY KEY (`DETALLEFAC_ID`),
-  KEY `FK_DETALLE_FACTURA_FACTURA` (`PRODUCTO_ID`),
-  KEY `FK_FACTURA_DETALLE_FACTURA` (`FACTURA_ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `detalle_pedido`
---
-
-DROP TABLE IF EXISTS `detalle_pedido`;
-CREATE TABLE IF NOT EXISTS `detalle_pedido` (
-  `DETALLEPEDIDO_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `PRODUCTO_ID` int(11) NOT NULL,
-  `PEDIDO_ID` int(11) NOT NULL,
-  `DETALLEPEDIDO_NROUNIDADES` decimal(10,0) NOT NULL,
-  PRIMARY KEY (`DETALLEPEDIDO_ID`),
-  KEY `FK_PEDIDO_DETALLE_PEDIDO` (`PEDIDO_ID`),
-  KEY `FK_PRODUCTO_DETALLE_PEDIDO` (`PRODUCTO_ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `detalle_id` int(11) NOT NULL AUTO_INCREMENT,
+  `producto_id` int(11) NOT NULL,
+  `factura_id` int(11) NOT NULL,
+  `iva` float NOT NULL,
+  `cantidad` float NOT NULL,
+  `precio` float NOT NULL,
+  PRIMARY KEY (`detalle_id`),
+  KEY `factura_id` (`factura_id`),
+  KEY `producto_id` (`producto_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -133,70 +65,32 @@ CREATE TABLE IF NOT EXISTS `detalle_pedido` (
 
 DROP TABLE IF EXISTS `factura`;
 CREATE TABLE IF NOT EXISTS `factura` (
-  `FACTURA_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `CLIENTE_ID` int(11) NOT NULL,
-  `USUARIO_ID` int(11) NOT NULL,
-  `USUARIO_CEDULA` varchar(10) NOT NULL,
-  `FACTURA_FECHA` timestamp NOT NULL,
-  `FACTURA_ESTADO` varchar(30) NOT NULL,
-  PRIMARY KEY (`FACTURA_ID`),
-  KEY `FK_FACTURA_CLIENTE` (`CLIENTE_ID`),
-  KEY `FK_RELATIONSHIP_3` (`USUARIO_ID`,`USUARIO_CEDULA`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `factura_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `factura_fecha` date NOT NULL,
+  PRIMARY KEY (`factura_id`),
+  KEY `usuario_id` (`user_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `inventario`
+-- Table structure for table `info_factura`
 --
 
-DROP TABLE IF EXISTS `inventario`;
-CREATE TABLE IF NOT EXISTS `inventario` (
-  `INVENTARIO_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `PRODUCTO_ID` int(11) NOT NULL,
-  `PROVEEDOR_ID` int(11) NOT NULL,
-  `PROVEEDOR_RUC` varchar(30) NOT NULL,
-  `INVENTARIO_NROUNIDADES` decimal(10,0) NOT NULL,
-  PRIMARY KEY (`INVENTARIO_ID`),
-  KEY `FK_INVENTARIO_PROVEEDOR` (`PROVEEDOR_ID`,`PROVEEDOR_RUC`),
-  KEY `FK_PRODUCTO_INVENTARIO` (`PRODUCTO_ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pedido`
---
-
-DROP TABLE IF EXISTS `pedido`;
-CREATE TABLE IF NOT EXISTS `pedido` (
-  `PEDIDO_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `USUARIO_ID` int(11) NOT NULL,
-  `USUARIO_CEDULA` varchar(10) NOT NULL,
-  `CLIENTE_ID` int(11) NOT NULL,
-  `PEDIDO_FECHA` timestamp NOT NULL,
-  `PEDIDO_FECHADESPACHO` timestamp NOT NULL,
-  `PEDIDO_ESTADO` varchar(30) NOT NULL,
-  `PEDIDO_OBSERVACION` varchar(100) NOT NULL,
-  `PEDIDO_FORMAENTREGA` varchar(30) NOT NULL,
-  `PEDIDO_REPONSABLEENTREGA` varchar(50) NOT NULL,
-  PRIMARY KEY (`PEDIDO_ID`),
-  KEY `FK_PEDIDO_CLIENTE` (`CLIENTE_ID`),
-  KEY `FK_USUARIO_PEIDO` (`USUARIO_ID`,`USUARIO_CEDULA`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `permiso`
---
-
-DROP TABLE IF EXISTS `permiso`;
-CREATE TABLE IF NOT EXISTS `permiso` (
-  `PERMISO_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `PERMISO_DESPERMISO` varchar(30) NOT NULL,
-  PRIMARY KEY (`PERMISO_ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `info_factura`;
+CREATE TABLE IF NOT EXISTS `info_factura` (
+  `info_factura_id` int(11) NOT NULL AUTO_INCREMENT,
+  `factura_id` int(11) NOT NULL,
+  `Nombres` varchar(50) NOT NULL,
+  `Apellidos` varchar(50) NOT NULL,
+  `nro_cedula` varchar(10) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `telefono` varchar(10) NOT NULL,
+  PRIMARY KEY (`info_factura_id`),
+  KEY `factura_id` (`factura_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -225,27 +119,6 @@ INSERT INTO `producto` (`PRODUCTO_ID`, `CATEGORY`, `DESCRIPCION`, `IMAGE`, `PREC
 (4, 'RAM Memory', 'SO-DIMM CORSAIR VENGEANCE 32GB (1X32GB) DDR4 2400 MHZ', 'http://localhost/Tienda-Virtual-2020/images/ram-1.jpg', 186.67),
 (5, 'RAM Memory', 'DIMM DELL 16GB 2Rx8 UDIMM 2666MHz ECC para T40. T140. R340', 'http://localhost/Tienda-Virtual-2020/images/ram-2.jpg', 220),
 (6, 'RAM Memory', 'DIMM DELL 16GB 2Rx8 RDIMM 2933MHZ ECC para R640. R740', 'http://localhost/Tienda-Virtual-2020/images/ram-3.jpg', 197.78);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `proveedor`
---
-
-DROP TABLE IF EXISTS `proveedor`;
-CREATE TABLE IF NOT EXISTS `proveedor` (
-  `PROVEEDOR_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `PROVEEDOR_NOMBRE` varchar(30) NOT NULL,
-  `PROVEEDOR_RUC` varchar(30) NOT NULL,
-  `PROVEEDOR_DIRECCION` varchar(50) NOT NULL,
-  `PROVEEDOR_CIUDAD` varchar(30) NOT NULL,
-  `PROVEEDOR_TLFCONVENCIONAL` decimal(15,0) NOT NULL,
-  `PROVEEDOR_TLFCELULAR` decimal(10,0) NOT NULL,
-  `PROVEEDOR_EMAIL` varchar(30) NOT NULL,
-  `PROVEEDOR_CONTACTO` varchar(30) DEFAULT NULL,
-  `PROVEEDOR_ESTADO` varchar(30) NOT NULL,
-  PRIMARY KEY (`PROVEEDOR_ID`,`PROVEEDOR_RUC`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -282,6 +155,19 @@ INSERT INTO `usuario` (`user_id`, `password`, `username`, `register_date`, `name
 ALTER TABLE `carrito`
   ADD CONSTRAINT `FK_PRODUCT` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`PRODUCTO_ID`),
   ADD CONSTRAINT `FK_USER` FOREIGN KEY (`user_id`) REFERENCES `usuario` (`user_id`);
+
+--
+-- Constraints for table `detalle_factura`
+--
+ALTER TABLE `detalle_factura`
+  ADD CONSTRAINT `FK_FACTURA` FOREIGN KEY (`factura_id`) REFERENCES `factura` (`factura_id`),
+  ADD CONSTRAINT `FK_PROD` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`PRODUCTO_ID`);
+
+--
+-- Constraints for table `info_factura`
+--
+ALTER TABLE `info_factura`
+  ADD CONSTRAINT `FK_FACTURA_INFO` FOREIGN KEY (`factura_id`) REFERENCES `factura` (`factura_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
